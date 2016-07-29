08e650764aca5fc0d8b03fd5403ac789c32e1114
riabaldoz <ria.baldoz@ge.com>
2016-07-29 08:54:21 -0700

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php   // db_config.php
+         define('DB_USER', 'root');
+         define('DB_PASSWORD', 'sanramon');
+         define('DB_DATABASE', 'ssomapping');
+         define('DB_SERVER', 'localhost');
+?>
